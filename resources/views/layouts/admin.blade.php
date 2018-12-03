3e9e89db86fd06857a0a0125e7b24bf2e1dec447
Evan <eevan4256@gmail.com>
2018-12-04 01:48:46 +0800

    Fixing Sidebar to display None when toggled Close

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,7 +34,7 @@
 
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
+                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -76,40 +76,42 @@
 
     </nav>
 <div id="mySidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <br>
-    <a class="nav-link ml-4 {{ Request::is('application') ? 'custom-red' : '' }}" href="{{ url('/application') }}">Application</a>
-    <a class="nav-link ml-4 {{ Request::is('accounts') ? 'custom-red' : '' }}" href="{{ url('/accounts') }}">Account</a>
-     <li class="dropdown">
-        <a  class="nav-link ml-2 dropdown-toggle" href="" data-toggle="collapse" data-target="#manage">Manage</a>
-        <div id="manage" class="collapse show">
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('authors') ? 'custom-red' : '' }}" id="link_author" href="{{ url('authors') }}" >
-                <i class="fas fa-user-tie mr-3"></i>Authors</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('books') ? 'custom-red' : '' }}" href="{{ url('books') }}">
-                <i class="fas fa-book mr-3"></i>Books</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('borrowers') ? 'custom-red' : '' }}" href="{{ url('borrowers') }}">
-                <i class="fas fa-book-reader mr-3"></i>Borrowers</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('categories') ? 'custom-red' : '' }}" href="{{ url('categories') }}">
-                <i class="fas fa-layer-group mr-3"></i>Categories</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('receives') ? 'custom-red' : '' }}" href="{{ url('receives') }}">
-                <i class="fas fa-undo-alt mr-3"></i>Returned</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('publishers') ? 'custom-red' : '' }}" href="{{ url('publishers') }}">
-                <i class="fas fa-file-upload mr-3"></i>Publishers</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('shelves') ? 'custom-red' : '' }}" href="{{ url('shelves') }}">
-                <i class="fas fa-archive mr-3"></i>Shelves</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('users') ? 'custom-red' : '' }}" href="{{ url('users') }}">
-                <i class="fas fa-users mr-3"></i>Users</a>
-        </div>
-    </li>
-    <li class="dropdown">
-        <a  class="nav-link ml-2 dropdown-toggle" href="#" data-toggle="collapse" data-target="#side_colapse">Action</a>
-        <div id="side_colapse" class="collapse show">
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('borrowers/create') ? 'custom-red' : '' }}" href="{{ url('borrowers/create') }}">
-                <i class="fas fa-share-square mr-3 "></i>Issue New Book</a>
-            <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('return') ? 'custom-red' : '' }}" href="{{ url('return') }}">
-                <i class="fas fa-check-circle mr-3"></i>Return Book</a>
-        </div>
-    </li>
+    <div class="sidenav_menu">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <br>
+        <a class="nav-link ml-4 {{ Request::is('application') ? 'custom-red' : '' }}" href="{{ url('/application') }}">Application</a>
+        <a class="nav-link ml-4 {{ Request::is('accounts') ? 'custom-red' : '' }}" href="{{ url('/accounts') }}">Account</a>
+         <li class="dropdown">
+            <a  class="nav-link ml-2 dropdown-toggle" href="" data-toggle="collapse" data-target="#manage">Manage</a>
+            <div id="manage" class="collapse show">
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('authors') ? 'custom-red' : '' }}" id="link_author" href="{{ url('authors') }}" >
+                    <i class="fas fa-user-tie mr-3"></i>Authors</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('books') ? 'custom-red' : '' }}" href="{{ url('books') }}">
+                    <i class="fas fa-book mr-3"></i>Books</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('borrowers') ? 'custom-red' : '' }}" href="{{ url('borrowers') }}">
+                    <i class="fas fa-book-reader mr-3"></i>Borrowers</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('categories') ? 'custom-red' : '' }}" href="{{ url('categories') }}">
+                    <i class="fas fa-layer-group mr-3"></i>Categories</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('receives') ? 'custom-red' : '' }}" href="{{ url('receives') }}">
+                    <i class="fas fa-undo-alt mr-3"></i>Returned</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('publishers') ? 'custom-red' : '' }}" href="{{ url('publishers') }}">
+                    <i class="fas fa-file-upload mr-3"></i>Publishers</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('shelves') ? 'custom-red' : '' }}" href="{{ url('shelves') }}">
+                    <i class="fas fa-archive mr-3"></i>Shelves</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('users') ? 'custom-red' : '' }}" href="{{ url('users') }}">
+                    <i class="fas fa-users mr-3"></i>Users</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a  class="nav-link ml-2 dropdown-toggle" href="#" data-toggle="collapse" data-target="#side_colapse">Action</a>
+            <div id="side_colapse" class="collapse show">
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('borrowers/create') ? 'custom-red' : '' }}" href="{{ url('borrowers/create') }}">
+                    <i class="fas fa-share-square mr-3 "></i>Issue New Book</a>
+                <a class="dropdown-item_side text-capitalize ml-2 {{ Request::is('return') ? 'custom-red' : '' }}" href="{{ url('return') }}">
+                    <i class="fas fa-check-circle mr-3"></i>Return Book</a>
+            </div>
+        </li>
+    </div>        
 </div>
 
 <div id="main">
@@ -123,24 +125,30 @@
 let screen = window.matchMedia("(max-width: 700px)")
 let Status = 'open';
 function openNav() {
+     let navItem = document.querySelector(".sidenav_menu");
+
     if (Status == 'close' && screen.matches) {
             document.getElementById("mySidenav").style.width = "100%";
-            Status = 'open'
+             navItem.style.display = 'block';
+            Status = 'open';
         }
     else if(Status == 'close'){
             document.getElementById("mySidenav").style.width = "220px";
             document.getElementById("main").style.marginLeft = "220px";
-            Status = 'open'
+             navItem.style.display = 'block';
+            Status = 'open';
             }
     else if (Status == 'open' && screen.matches) {
-             document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginLeft= "0";
-            Status = 'close'
+            navItem.style.display = 'none';
+            Status = 'close';
         }
     else{
         document.getElementById("mySidenav").style.width = "50px";
         document.getElementById("main").style.marginLeft= "50px";
-        Status = 'close'
+        navItem.style.display = 'none';
+        Status = 'close';
     }
    
 
