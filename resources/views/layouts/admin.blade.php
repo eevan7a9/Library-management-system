19b0a9da2313e42e2299d0184044b14ba901e644
code805 <gametester2020@tutanota.com>
2020-03-12 19:57:42 +0900

    fixed cors error

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
