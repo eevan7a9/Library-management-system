711168c96f6a3f4ceb6784d18ce3af944072774f
eevan7a9 <e7a9@protonmail.com>
2020-03-14 01:11:22 +0900

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
     }
 
     /**
