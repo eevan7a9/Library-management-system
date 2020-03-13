e7e55dbb96b205e1e6d58545920c6a638558723f
eevan7a9 <e7a9@protonmail.com>
2020-03-14 00:37:25 +0900

    added sample credentials on UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,29 +12,35 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right custom-font-cursive">{{ __('E-Mail Address') }}</label>
+                            <label for="email"
+                                class="col-sm-4 col-form-label text-md-right custom-font-cursive">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email"
+                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                    value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right custom-font-cursive">{{ __('Password') }}</label>
+                            <label for="password"
+                                class="col-md-4 col-form-label text-md-right custom-font-cursive">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" type="password"
+                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                    name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -42,7 +48,8 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
@@ -63,6 +70,46 @@
                             </div>
                         </div>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-8">
+            <div class="card">
+                <div class=" card-header text-danger">
+                    Sample credentials you can use:
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h5>Admin</h5>
+                            <p class="m-0 p-0">email:
+                                <code>admin@email.com</code>
+                            </p>
+                            <p class="m-0 p-0">Password:
+                                <code>password</code>
+                            </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h5>Librarian</h5>
+                            <p class="m-0 p-0">email:
+                                <code>naruto@email.net</code>
+                            </p>
+                            <p class="m-0 p-0">Password:
+                                <code>secret</code>
+                            </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <h5>Student</h5>
+                            <p class="m-0 p-0">email:
+                                <code>janedoe@email.com</code>
+                            </p>
+                            <p class="m-0 p-0">Password:
+                                <code>secret</code>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
